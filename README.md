7cc3d811645c8a678559313223bccccc638cfe90
WulfMarius <WulfMarius@users.noreply.github.com>
2019-01-18 21:56:23 +0100

    initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+Disable-Camera-Sway
+
+
+This is a mod for **The Long Dark** by Hinterland Studio, Inc.
+
+It prevents automatic camera movements in the absence of user input.
---
+++ src/Patches.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Harmony;
+
+namespace DisableCameraSway
+{
+     [HarmonyPatch(typeof(PlayerManager), "MaybeRotateCamera")]
+    internal class PlayerManager_MaybeRotateCamera
+    {
+        internal static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
